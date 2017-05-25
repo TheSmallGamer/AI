af1f675d54b92698c71e8423094b08f6ce547592
TripyVOX <tripyvox@gmail.com>
2017-05-25 14:50:22 -0500

    Massive Overhaul

--- MainGUI.java
+++ MainGUI.java
@@ -1,130 +1,153 @@
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.TextArea;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.beans.EventHandler;
+import java.util.Random;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.UIManager;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class MainGUI {
+public class CableBilling extends Application implements EventHandler<ActionEvent>{
+	
+	private Text nameText = new Text("Enter your name: ");
+	private TextField nameField = new TextField("");
+	private Text phraseText = new Text("Enter your text: ");
+	private static TextField phraseField = new TextField("");
+	private Button calc = new Button("Calculate Monthly Bill");
+	private Button clear = new Button("Clear");
+	private TextArea area = new TextArea("");
+	private String windowTitle = "AI - GUI Edition - Now 20% more GUI!";
 
-    MainGUI mainGUI;
-    JFrame newFrame = new JFrame("AI Chat v0.1");
-    Button sendMessage;
-    TextField messageBox = new TextField("");
-    TextArea chatBox = new TextArea("");
-    TextField usernameChooser = new TextField("");
-    JFrame preFrame;
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle(windowTitle);
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		calc.setOnAction(this);
+		clear.setOnAction(this);
+		grid.add(nameText, 0, 0);
+		grid.add(nameField, 1, 0);
+		grid.add(phraseText, 0, 1);
+		grid.add(phraseField, 1, 1);
+		grid.add(calc, 0, 6);
+		grid.add(area, 0, 5,2,1);
+		grid.add(clear, 1, 6);
+		area.setEditable(false);
+		primaryStage.setScene(new Scene(grid, 480,275));
+		primaryStage.show();
+		grid.setOnKeyPressed(new EventHandler<KeyEvent>(){
+			@Override
+			public void handle(KeyEvent key)
+			{
+				if (key.getCode().equals(KeyCode.ENTER))
+				{
+					area.appendText(nameField.getText());
+					nameField.setEditable(false);
+				}
+				else if (key.getCode().equals(KeyCode.C))
+				{
+					nameField.setEditable(true);
+					area.setText("");
+				}
+			}
+		});
+	}
+	
+	public void handle(ActionEvent event){
+		if(event.getSource() == calc)
+		{
+			area.appendText(nameField.getText());
+			nameField.setEditable(false);
+			area.appendText("Thank you for trying AI!\n");
+			area.appendText("Use one word command. Type help for help!\n");
+			area.appendText("If you want to chat, type 'convo'\n");
+			while (true) {
+				String ans;
+				ans = AI.process();
+				area.appendText(ans +"\n");
+			}
+		}
+		
+		if(event.getSource() == clear)
+		{
+			nameField.setEditable(true);
+			area.setText("");
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        MainGUI mainGUI = new MainGUI();
-        mainGUI.preDisplay();
-    }
+	public static String process() {
+		String feeling = "";
+		@SuppressWarnings("resource")
+		String current;
+		System.out.print("> ");
+		current = phraseField.getText();
+		current = current.toLowerCase();
+			while (true) {
+				String current2;
+				System.out.print(">> ");
+				current2 = scan.nextLine();
+				current2 = current2.toLowerCase();
+				if (current2.compareTo("") != 0) {
+					if (search("hello", current2) == true || search("hi", current2) == true ) {
+						return("Hello There! I am Reminiscence the Artificial Changeling!");
+					} else if ((search("how", current2) == true && search("are", current2) == true && search("you", current2) == true) ||(search("how", current2) == true && search("is", current2) == true) ) {
+						String blank = "";
+							Random rand = new Random();
+							int  n = rand.nextInt(3);
+							if (n == 0) {
+								if (feeling.compareTo(blank) == 0) {
+									feeling = "kind of sad.";
+									return("I am feeling " + feeling);
+								} else {
+									return("I already told you I'm " + feeling);
+								}
+							} else if (n == 1) {
+								if (feeling.compareTo(blank) == 0) {
+									feeling = "very happy today!";
+									return("I am feeling " + feeling);
+								} else {
+									return("I already told you I'm " + feeling);
+								}	
+							} else if (n == 2) {
+								if (feeling.compareTo(blank) == 0) {
+									feeling = "depressed...";
+									return("I am feeling " + feeling);
+								} else {
+									return("I already told you I'm " + feeling);
+								}
+							} else {
+								if (feeling.compareTo(blank) == 0) {
+									feeling = "alright.";
+									return"I am feeling " + feeling;
+								} else {
+									return("I already told you I'm " + feeling);
+								}	
+							}
+							
+					} else {
+						return("I don't think I understood... perhaps you could rephrase your question?");
+					}
+				} 
+			}
+	}
+	public static Boolean search(String keyword, String phrase) {
+			if (phrase.indexOf(keyword) >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+	}
 
-
-    public void preDisplay() {
-        newFrame.setVisible(false);
-        preFrame = new JFrame("Choose your username! (AI Chat Room)");
-        usernameChooser = new TextField();
-        JLabel chooseUsernameLabel = new JLabel("Pick a username:");
-        Button enterServer = new Button("Enter Chat Server");
-        JPanel prePanel = new JPanel(new GridBagLayout());
-
-        GridBagConstraints preRight = new GridBagConstraints();
-        preRight.anchor = GridBagConstraints.EAST;
-        GridBagConstraints preLeft = new GridBagConstraints();
-        preLeft.anchor = GridBagConstraints.WEST;
-        preRight.weightx = 5.0;
-        preRight.fill = GridBagConstraints.HORIZONTAL;
-        preRight.gridwidth = GridBagConstraints.REMAINDER;
-
-        prePanel.add(chooseUsernameLabel, preLeft);
-        prePanel.add(usernameChooser, preRight);
-        preFrame.add(BorderLayout.CENTER, prePanel);
-        preFrame.add(BorderLayout.SOUTH, enterServer);
-        preFrame.setVisible(true);
-        preFrame.setSize(400, 300);
-
-        enterServer.addActionListener(new enterServerButtonListener());
-    }
-
-    public void display() {
-        newFrame.setVisible(true);
-        JPanel southPanel = new JPanel();
-        newFrame.add(BorderLayout.SOUTH, southPanel);
-        southPanel.setBackground(Color.RED);
-        southPanel.setLayout(new GridBagLayout());
-
-        messageBox = new TextField((int) 43.5);
-        Button b = new Button("Send Message");
-        sendMessage = b;
-        
-        
-        
-       
-        
-        chatBox = new TextArea("Thank you for trying AI\nIf you want to chat, type 'convo'\nIf you need help, type 'help'\nIf you have any questions, type 'summary'\nIf you want to clear your messages, type '/clear'\n--------------------------------------------------------------------------------------\n\n");
-        chatBox.setEditable(false);
-        newFrame.add(new JScrollPane(chatBox), BorderLayout.CENTER);
-
-       
-
-        GridBagConstraints left = new GridBagConstraints();
-        GridBagConstraints right = new GridBagConstraints();
-        right.anchor = GridBagConstraints.EAST;
-        right.weightx = 2.0;
-
-        southPanel.add(messageBox, left);
-        southPanel.add(sendMessage, right);
-
-        chatBox.setFont(new Font("Serif", Font.PLAIN, 15));
-        sendMessage.addActionListener(new sendMessageButtonListener());
-        newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        newFrame.setSize(470, 300);
-    }
-
-    class sendMessageButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            if (messageBox.getText().length() < 1) {
-                // do nothing 
-            } else if (messageBox.getText().equals("/clear")) {
-                chatBox.setText("Thank you for trying AI\nIf you want to chat, type 'convo'\nIf you need help, type 'help'\nIf you have any questions, type 'summary'\nIf you want to clear your messages, type '/clear'\n--------------------------------------------------------------------------------------\n\n");
-                messageBox.setText("");
-            } else {
-                chatBox.append("<" + username + ">:  " + messageBox.getText() + "\n");
-                messageBox.setText("");
-            }
-        }
-    }
-
-    String username;
-
-    class enterServerButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            username = usernameChooser.getText();
-            if (username.length() < 1) {System.out.println("No!"); }
-            else {
-            preFrame.setVisible(false);
-            display();
-            }
-        }
-
-    }
 }
